ea3bfcda3f6b78f2a275b45a7605f05d288acda5
Christian <vairacing@gmail.com>
2026-05-16 11:19:22 +0200

    Bump Android release version to 0.1.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "tech.vairacing.redriver2"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
 
         ndk {
             abiFilters += "arm64-v8a"
